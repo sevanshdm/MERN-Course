97b573629cb9b48986ac3961cedd8aacdbeedffe
sevans <sevans@highdesertmilk.com>
2023-11-01 12:38:46 -0600

    Pages Created

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+// import React from 'react' You don't have to import React anymore
+
+function Dashboard() {
+    return (
+        <div>Dashboard</div>
+    )
+}
+
+export default Dashboard
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+function Register() {
+    return (
+        <div>register</div>
+    )
+}
+
+export default Register
